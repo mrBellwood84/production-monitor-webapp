68939cc03d4bd1b10e7d51e9dab301eaafe32c15
Kristian Wessel Finstad <kristian.w.finstad@gmail.com>
2023-03-01 23:01:16 +0100

    gauge tweeked

--- src/Views/CanvasComponents/lib/drawGauge.ts
+++ src/Views/CanvasComponents/lib/drawGauge.ts
@@ -1,3 +1,4 @@
+import { grid } from "@mui/system";
 import { IGaugeProps } from "./IGaugeProps"
 import { IGaugeValues } from "./IGaugeValues"
 
@@ -8,10 +9,9 @@ export const drawDoubleGauge = (
 ) => {
 
     try {
-        ctx.clearRect(0, 0, props.width, props.height)
-        ctx.translate(props.center_x, props.center_y);
+        ctx.clearRect(0, 0, props.width * 2, props.height * 2)
+        ctx.translate(props.center_x, props.center_y + (props.center_y / 20));
 
-        ctx.shadowBlur = 0;
         drawHeader(ctx, props)
 
         drawMainGaugePart(ctx, props, 0.75 * Math.PI, 1.25 * Math.PI, "red")
@@ -22,28 +22,24 @@ export const drawDoubleGauge = (
         drawTopGaugePart(ctx, props, (1.25 + (0.5 / 3)) * Math.PI, (1.25 + (0.5 / 3 * 2)) * Math.PI, "yellow")
         drawTopGaugePart(ctx, props, (1.25 + (0.5 / 3 * 2)) * Math.PI, 1.75 * Math.PI, "green" );
 
-        ctx.shadowColor = "#555"
-        ctx.shadowBlur = 2;
-
         drawMainHand(ctx,props, values)
         drawTopHand(ctx,props, values)
 
-        ctx.shadowBlur = 0;
         drawBottomText(ctx, props, values)
 
-        ctx.translate(-props.center_x, -props.center_y);
-    } catch { }
+        ctx.translate(-props.center_x, -(props.center_y + (props.center_y / 20)));
+    } catch {}
 }
 
 const drawHeader = (
     ctx: CanvasRenderingContext2D,
     props: IGaugeProps
 ) => {
-    const fontSize = props.mainRadius / 2.2;
+    const fontSize = props.mainRadius / 2.8;
 
     ctx.font = `${fontSize}px Roboto`
     ctx.textAlign = "center"
-    ctx.fillText(props.name, 0, - props.boxRadius / 1.16);
+    ctx.fillText(props.name, 0, - props.boxRadius / 1.10);
 }
 
 const drawBottomText = (
@@ -108,7 +104,7 @@ const drawMainHand = (
     ctx.beginPath();
     ctx.arc(0,0, props.handbaseRadius, 0, 1 * Math.PI);
     ctx.arc(0, -props.mainRadius, (props.handbaseRadius / 4), 1 * Math.PI, 0)
-    ctx.fillStyle = "#112";
+    ctx.fillStyle = "#333";
     ctx.fill();
 
     ctx.rotate(-rotation)
@@ -124,11 +120,32 @@ const drawTopHand = (
     ctx.rotate(rotation);
 
     ctx.beginPath();
-    ctx.moveTo(0, -(props.topRadius! - (props.topRadius! / 11)))
-    ctx.lineTo(0, -(props.topRadius! + (props.topRadius! / 11)))
-    ctx.strokeStyle = "#112";
-    ctx.lineWidth = props.topRadius! / 11;
-    ctx.stroke();
+
+    const x_outerRight = props.boxRadius / 40;
+    const x_outerLeft = -x_outerRight;
+    const x_innerRight = props.boxRadius / 150;
+    const x_innerLeft = -x_innerRight;
+
+    const y_innerBottom = -(props.topRadius! - (props.topRadius! / 11));
+    const y_innerTop = -(props.topRadius! + (props.topRadius! / 11));
+    const y_outerTop = y_innerTop - (props.topRadius! / 25);
+    const y_outerBottom = y_innerBottom + (props.topRadius! / 25);
+
+    ctx.moveTo(x_outerLeft, y_outerBottom);
+    ctx.lineTo(x_outerRight, y_outerBottom);
+    ctx.lineTo(x_innerRight, y_innerBottom);
+    ctx.lineTo(x_innerRight, y_innerTop);
+    // ctx.lineTo(x_outerRight, y_outerTop);
+    // ctx.lineTo(x_outerLeft, y_outerTop);
+    ctx.lineTo(x_innerLeft, y_innerTop);
+    ctx.lineTo(x_innerLeft, y_innerBottom);
+    ctx.lineTo(x_outerLeft, y_outerBottom)
+
+    // ctx.moveTo(0, -(props.topRadius! - (props.topRadius! / 11)))
+    // ctx.lineTo(0, -(props.topRadius! + (props.topRadius! / 11)))
+    ctx.fillStyle = "#333";
+    ctx.lineWidth = 1;
+    ctx.fill()
 
     ctx.rotate(-rotation);
 }
@@ -139,20 +156,20 @@ const createLinearGradient = (
     gridBase: number,
 ) => {
     if (color === "red") {
-        const grad = ctx.createLinearGradient(0, 0, -gridBase, 0);
-        grad.addColorStop(0, "#cc0000");
-        grad.addColorStop(1, "#ff3333");
+        const grad = ctx.createLinearGradient(0, 0, gridBase, 0);
+        grad.addColorStop(0, "#ff0000");
+        grad.addColorStop(1, "#FF6347");
         return grad;
     }
     if (color === "yellow") {
-        const grad = ctx.createLinearGradient(0,0, 0, -gridBase);
-        grad.addColorStop(0, "#e6e600");
-        grad.addColorStop(1, "#ffff80");
+        const grad = ctx.createLinearGradient(0,0, gridBase, 0);
+        grad.addColorStop(0, "#ffff00");
+        grad.addColorStop(1, "#ffffe5");
         return grad;
     }
 
     if (color === "green") {
-        const grad = ctx.createLinearGradient(gridBase, 0, 0, 0);
+        const grad = ctx.createLinearGradient(0, 0, gridBase, 0);
         grad.addColorStop(0, "#009900")
         grad.addColorStop(1, "#006600")
         return grad;
